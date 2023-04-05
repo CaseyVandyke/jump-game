db653d9c7596878749dcbee731595ddc5b35e25b
CaseyVandyke <caseyvandyke2@gmail.com>
2023-04-05 14:48:17 -0600

    added multiple platforms and sprite

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,19 +7,27 @@ export default class Game extends Phaser.Scene {
 
     preload = () => {
         this.load.image('background', 'assets/bg_layer1.png')
-        const grass = this.load.image('platform', 'assets/ground_grass.png');
+        this.load.image('platform', 'assets/ground_grass.png');
+
+        this.load.image('bunny-stand', 'assets/bunny1_stand.png')
+
     }
 
     create = () => {
         this.add.image(240, 320, 'background');
         const platforms = this.physics.add.staticGroup()
-        console.log(platforms);
 
         for (let i = 0; i < 5; ++i) {
             const x = Phaser.Math.Between(80, 400)
             const y = 150 * i;
 
             const platform = platforms.create(x, y, 'platform')
+            platform.scale = 0.5;
+
+            const body = platform.body
+            body.updateFromGameObject()
         }
+
+        this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
     }
 }
