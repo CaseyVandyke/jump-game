68b8d96712239987e9e9f8f1746e5deb1a9f567c
CaseyVandyke <caseyvandyke2@gmail.com>
2023-04-11 20:24:45 -0600

    changes

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -19,13 +19,14 @@ export default class Game extends Phaser.Scene {
 
     create = () => {
         this.add.image(240, 320, 'background');
-        const platforms = this.physics.add.staticGroup()
+        this.platforms = this.physics.add.staticGroup()
 
+        // then create 5 platforms from the group
         for (let i = 0; i < 5; ++i) {
             const x = Phaser.Math.Between(80, 400);
             const y = 150 * i;
 
-            const platform = platforms.create(x, y, 'platform');
+            const platform = this.platforms.create(x, y, 'platform');
             platform.scale = 0.5;
 
             const body = platform.body;
